573d0b70592dfb6206e7d3659d51d1e5d657f021
Nick Korotysh <kolchaprogrammer@list.ru>
2018-12-06 15:22:20 +0300

    get device serial number
    Change-Id: I48996e35b0de04601a9b46d036b636811c71c5b6
    Signed-off-by: Nick Korotysh <kolchaprogrammer@list.ru>

--- src/com/android/settings/deviceinfo/SerialNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/SerialNumberPreferenceController.java
@@ -25,14 +25,47 @@ import android.text.TextUtils;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.settings.core.PreferenceController;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import java.util.concurrent.Semaphore;
+import java.net.InetSocketAddress;
+import android.net.LocalSocketAddress;
+import android.net.LocalSocket;
+import java.net.Socket;
+import android.util.Log;
+
 public class SerialNumberPreferenceController extends PreferenceController {
+    private String TAG = "Settings_Status";
 
     private static final String KEY_SERIAL_NUMBER = "serial_number";
 
-    private final String mSerialNumber;
+    private String mSerialNumber;
 
     public SerialNumberPreferenceController(Context context) {
-        this(context, Build.getSerial());
+        super(context);
+        // //////////////////////////////////////////////
+        // MF0300
+
+        Log.i(TAG, "*** TcpClientThread BEFORE");
+        tcpClientThread = new TcpClientThread();
+        tcpClientThread.start();
+        Log.i(TAG, "*** TcpClientThread AFTER");
+
+        try {
+                sema_sync.acquire();
+        } catch(Exception e) {
+                Log.e(TAG, "*** UNABLE GET HW:SERIALNO EXCEPTION" + e.getMessage());
+                e.printStackTrace();
+        }
+        // //////////////////////////////////////////////
+
+        //String serial = Build.SERIAL;
+        Log.i(TAG, "*** serial:" + mSerialNumber);
     }
 
     @VisibleForTesting
@@ -59,4 +92,63 @@ public class SerialNumberPreferenceController extends PreferenceController {
     public String getPreferenceKey() {
         return KEY_SERIAL_NUMBER;
     }
+
+    class TcpClientThread extends Thread {
+        TcpClientThread() {
+        }
+
+        public void run() {
+                try {
+                        String sernum="cmd::get::";
+
+                        LocalSocket socket;
+                        LocalSocketAddress localsocketaddr;
+                        InputStream is;
+                        OutputStream os;
+                        DataInputStream dis;
+                        PrintStream ps;
+                        BufferedReader br;
+                        Log.i(TAG, "*** HWSER LocalSocket");
+                        socket = new LocalSocket();
+                        localsocketaddr = new LocalSocketAddress("serialnumber",LocalSocketAddress.Namespace.RESERVED);
+                        socket.connect(localsocketaddr);
+                        is = socket.getInputStream();
+                        os = socket.getOutputStream();
+                        dis = new DataInputStream(is);
+                        ps = new PrintStream(os);
+
+                        Log.i(TAG, "*** HWSER getBytes");
+
+                        byte[] msg1 = sernum.getBytes();
+                        ps.write(msg1);
+                        InputStream in = socket.getInputStream();
+                        br = new BufferedReader(new InputStreamReader(in));
+                        if (sernum.endsWith("get::")) {
+                                StringBuffer strBuffer = new StringBuffer();
+                                char c = (char) br.read();
+                                while (c != 0xffff) {
+                                        strBuffer.append(c);
+                                        c=(char) br.read();
+                                }
+                                mSerialNumber=strBuffer.toString();
+                                sema_sync.release();
+                        }
+
+			Log.i(TAG, "*** HWSER getBytes DONE serial:" + mSerialNumber);
+
+                        dis.close();
+                        ps.close();
+                        is.close();
+                        os.close();
+                        socket.close();
+                } catch (Exception e) {
+                        Log.e(TAG, "*** ENABLE GET HW:SERIAL FROMSOCKET EXCEPTION " + e.getMessage());
+                        e.printStackTrace();
+
+                }
+        }
+    }
+
+    private TcpClientThread tcpClientThread = null;
+    private final Semaphore sema_sync = new Semaphore(0, true);
 }
